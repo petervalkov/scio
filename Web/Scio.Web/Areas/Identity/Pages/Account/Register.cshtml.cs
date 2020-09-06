f97443c6748f393f740a2bf94aacb77106037eb2
Peter Valkov <petevalkovv@gmail.com>
2020-09-07 01:40:01 +0300

    Add account.css, change Register form validation messages, small layout fixes

--- Web/Scio.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/Scio.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -60,14 +60,14 @@
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "{0} must be between {2} and {1} characters.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Passwords don't match.")]
             public string ConfirmPassword { get; set; }
         }
 
